92683d3a7e4d7cb0a55f567f4158525159b95545
OjagaO <ahiroto1234@gmail.com>
2023-12-12 22:13:53 +0900

    category[id].tsx blogs nothing patern add

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -11,10 +11,6 @@ import * as Icon from "react-feather";
 import Pagenation from "@/components/Pagenation";
 
 export default function CategoryId({ blogs, totalCount, category, tag }: { blogs: Blogs[]; totalCount: number; category: Categories[]; tag: Tags[] }) {
-    // カテゴリーに紐付いたコンテンツがない場合に表示
-    if (!blogs) {
-        return <div>ブログコンテンツがありません</div>;
-    }
 
     const [contentHeight, setContentHeight] = useState(0);
 
@@ -32,32 +28,36 @@ export default function CategoryId({ blogs, totalCount, category, tag }: { blogs
                 <article className={styles.contents_main}>
                     <p className={styles.article_list}>記事の一覧</p>
                     <ul>
-                        {blogs.map((blog) => (
-                            <Link href={`/blog/${blog.id}`} key={blog.id}>
-                                <li className={styles.article_card}>
-                                    <Image className={styles.thumbnail} src={blog.eyecatch.url} alt="サムネイル" width={300} height={150} priority />
-                                    <div className={styles.article_card_right}>
-                                        <h3 className={styles.article_title}>{blog.title}</h3>
-                                        <div className={`${styles.flex_center} ${styles.mt_12} ${styles.sp_colum}`}>
-                                            <span className={styles.category}>{blog.category.name}</span>
-                                            <span className={styles.flex_center}>
-                                                {blog.tag.map((tagItem) => (
-                                                    <div className={`${styles.tag} ${styles.flex_center}`} key={tagItem.tag}>
-                                                        <Icon.Tag />
-                                                        <p>{tagItem.tag}</p>
-                                                    </div>
-                                                ))}
-                                            </span>
+                        {blogs ? (
+                            blogs.map((blog) => (
+                                <Link href={`/blog/${blog.id}`} key={blog.id}>
+                                    <li className={styles.article_card}>
+                                        <Image className={styles.thumbnail} src={blog.eyecatch.url} alt="サムネイル" width={300} height={150} priority />
+                                        <div className={styles.article_card_right}>
+                                            <h3 className={styles.article_title}>{blog.title}</h3>
+                                            <div className={`${styles.flex_center} ${styles.mt_12} ${styles.sp_colum}`}>
+                                                <span className={styles.category}>{blog.category.name}</span>
+                                                <span className={styles.flex_center}>
+                                                    {blog.tag.map((tagItem) => (
+                                                        <div className={`${styles.tag} ${styles.flex_center}`} key={tagItem.tag}>
+                                                            <Icon.Tag />
+                                                            <p>{tagItem.tag}</p>
+                                                        </div>
+                                                    ))}
+                                                </span>
+                                            </div>
+                                            <div className={`${styles.flex_center} ${styles.mt_12}`}>
+                                                <Icon.Clock />
+                                                <time className={styles.date}>{blog.publishedAt.slice(0, 10).replace(/-/g, "/")}</time>
+                                            </div>
                                         </div>
-                                        <div className={`${styles.flex_center} ${styles.mt_12}`}>
-                                            <Icon.Clock />
-                                            <time className={styles.date}>{blog.publishedAt.slice(0, 10).replace(/-/g, "/")}</time>
-                                        </div>
-                                    </div>
-                                    {/* <div className="blog-content" dangerouslySetInnerHTML={{ __html: blog.content }} /> */}
-                                </li>
-                            </Link>
-                        ))}
+                                    </li>
+                                </Link>
+                            ))
+                        ) : (
+                            <div>ブログコンテンツがありません</div>
+                        )}
+
                     </ul>
 
                     <Pagenation totalCount={totalCount} />
